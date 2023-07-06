9ca8c5061b2207cb5920340b8fb8f51a784e9458
FELIPE DA SILVA ALVES <felipe.salves14@senacsp.edu.br>
2023-07-06 14:21:34 -0300

    Calculadora que só soma.

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -39,68 +39,78 @@
             // 
             // soma
             // 
-            soma.Location = new Point(77, 199);
+            soma.Location = new Point(96, 249);
+            soma.Margin = new Padding(4, 4, 4, 4);
             soma.Name = "soma";
-            soma.Size = new Size(94, 29);
+            soma.Size = new Size(118, 36);
             soma.TabIndex = 0;
             soma.Text = "Soma";
             soma.UseVisualStyleBackColor = true;
+            soma.Click += soma_Click;
             // 
             // primeiro_numero
             // 
             primeiro_numero.AutoSize = true;
-            primeiro_numero.Location = new Point(12, 64);
+            primeiro_numero.Location = new Point(15, 80);
+            primeiro_numero.Margin = new Padding(4, 0, 4, 0);
             primeiro_numero.Name = "primeiro_numero";
-            primeiro_numero.Size = new Size(120, 20);
+            primeiro_numero.Size = new Size(145, 25);
             primeiro_numero.TabIndex = 1;
             primeiro_numero.Text = "Primeiro número";
             // 
             // segundo_numero
             // 
             segundo_numero.AutoSize = true;
-            segundo_numero.Location = new Point(12, 109);
+            segundo_numero.Location = new Point(15, 136);
+            segundo_numero.Margin = new Padding(4, 0, 4, 0);
             segundo_numero.Name = "segundo_numero";
-            segundo_numero.Size = new Size(123, 20);
+            segundo_numero.Size = new Size(151, 25);
             segundo_numero.TabIndex = 2;
             segundo_numero.Text = "Segundo número";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(141, 64);
+            textBox1.Location = new Point(176, 80);
+            textBox1.Margin = new Padding(4, 4, 4, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
+            textBox1.Size = new Size(155, 31);
             textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(141, 106);
+            textBox2.Location = new Point(176, 132);
+            textBox2.Margin = new Padding(4, 4, 4, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
+            textBox2.Size = new Size(155, 31);
             textBox2.TabIndex = 4;
             // 
             // resultado
             // 
             resultado.AutoSize = true;
-            resultado.Location = new Point(12, 157);
+            resultado.Location = new Point(15, 196);
+            resultado.Margin = new Padding(4, 0, 4, 0);
             resultado.Name = "resultado";
-            resultado.Size = new Size(75, 20);
+            resultado.Size = new Size(90, 25);
             resultado.TabIndex = 5;
             resultado.Text = "Resultado";
             // 
             // resultado_mostra
             // 
             resultado_mostra.AutoSize = true;
-            resultado_mostra.Location = new Point(141, 157);
+            resultado_mostra.Location = new Point(176, 196);
+            resultado_mostra.Margin = new Padding(4, 0, 4, 0);
             resultado_mostra.Name = "resultado_mostra";
-            resultado_mostra.Size = new Size(17, 20);
+            resultado_mostra.Size = new Size(22, 25);
             resultado_mostra.TabIndex = 6;
             resultado_mostra.Text = "0";
+            resultado_mostra.Click += resultado_mostra_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(278, 395);
+            ClientSize = new Size(348, 494);
             Controls.Add(resultado_mostra);
             Controls.Add(resultado);
             Controls.Add(textBox2);
@@ -108,6 +118,7 @@
             Controls.Add(segundo_numero);
             Controls.Add(primeiro_numero);
             Controls.Add(soma);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -6,5 +6,25 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resultado_mostra_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void soma_Click(object sender, EventArgs e)
+        {
+            int primerio = Int32.Parse(textBox1.Text);
+            int segundo = Int32.Parse(textBox2.Text);
+            int conta = primerio + segundo;
+
+            resultado_mostra.Text = conta.ToString();
+
+        }
     }
 }
